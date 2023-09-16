df9040955e072782988766568f64b3f34cc50057
Jessica Wang <76851878+asTravars33@users.noreply.github.com>
2023-09-16 13:25:27 -0400

    Update hackcmu.ino

--- hackcmu.ino
+++ hackcmu.ino
@@ -4,31 +4,69 @@
 int moisture = 0;
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
+
+
+
 void setup()
 {
   pinMode(A0, OUTPUT);
   pinMode(A1, INPUT);
   Serial.begin(9600);
 
+
+
+
 }
+
+
+
 
 void loop()
 {
     float red, green, blue;
-  
+ 
   tcs.setInterrupt(false);  // turn on LED
+
+
+
 
   delay(60);  // takes 50ms to read
 
-  tcs.getRGB(&red, &green, &blue);
 
-  
-  
+
+
+  tcs.getRGB(&red, &green, &blue);
+ 
+  if(blue > 210){
+    Serial.println("hydrated");
+  }
+    else{
+      if(blue > 120){
+        Serial.println("average");
+      }
+        else{
+          Serial.println("dehydrated");
+        }
+    }
+   
+ 
+
+
+
+
+ 
+ 
   tcs.setInterrupt(true);  // turn off LED
 
-  Serial.print("R:\t"); Serial.print(int(red)); 
-  Serial.print("\tG:\t"); Serial.print(int(green)); 
+
+
+
+  Serial.print("R:\t"); Serial.print(int(red));
+  Serial.print("\tG:\t"); Serial.print(int(green));
   Serial.print("\tB:\t"); Serial.print(int(blue));
+
+
+
 
 //  Serial.print("\t");
 //  Serial.print((int)red, HEX); Serial.print((int)green, HEX); Serial.print((int)blue, HEX);
@@ -42,22 +80,43 @@ void loop()
   digitalWrite(A0, LOW);
 
 
+
+
+
+
+
+
   if (moisture < 350) {
     Serial.println("compaction");
+
+
+
 
   } else {
     if (moisture < 600) {
       Serial.println("constipation");
 
+
+
+
     } else {
       if (moisture < 700) {
         Serial.println("normal");
+
+
+
 
       } else {
         if (moisture < 1000) {
           Serial.println("diarrhea");
 
+
+
+
         } else {
+
+
+
 
         }
       }
@@ -65,3 +124,4 @@ void loop()
   }
   delay(100); // Wait for 100 millisecond(s)
 }
+
